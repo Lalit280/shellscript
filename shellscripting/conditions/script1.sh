7d558b644ae7aea39d72c2322c9c2b97db6de938
lalit280 <beheralalit13@gmail.com>
2024-01-22 07:41:08 +0000

    adding all files

---
+++ shellscripting/conditions/script1.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+#
+#
+#
+echo "enetr salary"
+read sal
+if [ $sal -lt 20000 ];
+then
+    echo "No Tax"
+elif [ $sal -gt 20000 -a $sal -lt 40001 ];
+then
+    echo "Tax is 5%"
+elif [ $sal -ge 40000 ];
+then
+    echo "Tax is 10%"
+elif [ $sal -eq 20000 ]
+then
+    echo "Tax is 2%"
+else
+    echo "Invalid salary entered"
+fi
+
